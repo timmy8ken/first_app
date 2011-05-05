f8b45f80a55d8218e23290546b1e1d4b8599b477
Tim Aitken <timmy8ken@gmail.com>
2011-05-05 15:46:58 +1000

    Altered env.rb for first_App v2

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,6 @@ if ENV['RAILS_ENV'] == 'production'  # don't bother on dev
   ENV['GEM_PATH'] = '/home/timmy8ken/.gems' + ':/usr/lib/ruby/gems/1.8'
 end
 
-require File.join(File.dirname(__FILE__), 'boot')
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
